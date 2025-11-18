f3c9f538f1e79f0039da92be9dd0ce65652e9789
Thumula <thumuladgamage@gmail.com>
2025-11-18 22:49:59 +0530

    merge  update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
  // app/_layout.tsx
+
 import { useState, useEffect } from "react";
 import { Stack, useRouter, useSegments } from "expo-router";
 import { UserProvider } from "../context/UserDetailContext";
